7807747d54792c8c9fb49d2fcf64dccb756d62c0
JulesPomme <julian.moreira.fr@gmail.com>
2018-05-07 07:27:38 +0200

    Initial commit

---
+++ Assets/Scripts/CustomPhysics.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomPhysics : MonoBehaviour {
+
+    public float minGroundNormalY = .65f;
+    public float gravityModifier = 1f;
+
+    protected bool grounded;
+    protected Vector2 targetVelocity;
+    protected Vector2 groundNormal;
+    protected Rigidbody2D rb2d;
+    protected Vector2 velocity;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+
+    void OnEnable() {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Start() {
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+    }
+
+    protected virtual float ComputeVelocity() {
+        return 0f;
+    }
+
+    void FixedUpdate() {
+
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        velocity.x = ComputeVelocity();
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        grounded = false;
+
+        Vector2 movementAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+        Vector2 movement = movementAlongGround * deltaPosition.x;
+        Move(movement, false);
+
+        movement = Vector2.up * deltaPosition.y;
+        Move(movement, true);
+    }
+
+    void Move(Vector2 move, bool yMovement) {
+        float distance = move.magnitude;
+
+        if (distance > minMoveDistance) {
+            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+
+            for (int i = 0; i < count; i++) {
+                Vector2 currentNormal = hitBuffer[i].normal;
+                if (currentNormal.y > minGroundNormalY) {
+                    grounded = true;
+                    if (yMovement) {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if (projection < 0) {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBuffer[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+
+
+        }
+
+        rb2d.position = rb2d.position + move.normalized * distance;
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : CustomPhysics {
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    private SpriteRenderer spriteRenderer;
+    private float hInput;
+    private bool jumpInputDown;
+    private bool jumpInputUp;
+
+    void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update() {
+
+        hInput = Input.GetAxis("Horizontal");
+        jumpInputDown = Input.GetKeyDown(KeyCode.RightControl);
+        jumpInputUp = Input.GetKeyUp(KeyCode.RightControl);
+    }
+
+    protected override float ComputeVelocity() {
+        Vector2 movement = Vector2.zero;
+
+        movement.x = hInput;
+
+        if (jumpInputDown && grounded) {
+            velocity.y = jumpTakeOffSpeed;
+        } else if (jumpInputUp && velocity.y > 0) {
+            velocity.y = velocity.y * 0.5f;
+        }
+
+        bool flipSprite = (spriteRenderer.flipX ? (movement.x > 0.01f) : (movement.x < 0.01f));
+        if (flipSprite) {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        return (movement * maxSpeed).x;
+    }
+}
